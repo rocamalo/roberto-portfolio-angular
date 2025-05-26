dcc9d800c6cece146126455499590d76c4206b28
rml1 <roberto.martinez.contractor@akc.org>
2025-05-26 17:41:41 -0600

    english default language in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,7 +53,7 @@ export function HttpLoaderFactory(http: HttpClient) {
         useFactory: HttpLoaderFactory,
         deps: [HttpClient]
       },
-      defaultLanguage: 'es'
+      defaultLanguage: 'en'
     })
   ],
   providers: [],
