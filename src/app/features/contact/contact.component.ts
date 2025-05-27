0b46570069f47f4ebdb444f5321d27e95f3831b1
rml1 <roberto.martinez.contractor@akc.org>
2025-05-26 19:31:08 -0600

    Testing netlify forms

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,24 +6,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
-  showSuccessMessage = false;
-
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    // Verificar si hay parámetro de éxito en la URL
-    this.route.queryParams.subscribe(params => {
-      if (params['success'] === 'true') {
-        this.showSuccessMessage = true;
-
-        // Ocultar mensaje después de 5 segundos
-        setTimeout(() => {
-          this.showSuccessMessage = false;
-          // Limpiar URL
-          window.history.replaceState({}, document.title, window.location.pathname);
-        }, 5000);
-      }
-    });
-  }
+export class ContactComponent {
+  // Componente súper simple, sin lógica de formulario
+  // Netlify maneja todo
 }
